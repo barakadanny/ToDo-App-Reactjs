3c6b92069d0ff1fc1031b089bc257ffd32b2f135
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-12 15:01:47 +0300

    InputTodo function created

---
+++ src/components/InputTodo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function InputTodo() {
+  return (
+    <form>
+      <input type='text' placeholder='Add Todo...' />
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default InputTodo;
