4567f9b31ab461c34c78d396b9c3ea63c73612ac
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-12 18:59:56 +0300

    Display Added todo to Dom

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 
-function InputTodo() {
-  return (
-    <form>
-      <input type='text' placeholder='Add Todo...' />
-      <button>Submit</button>
-    </form>
-  );
+class InputTodo extends Component {
+  state = {
+    title: '',
+  };
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.title);
+    this.props.addTodoProps(this.state.title);
+    this.setState({
+      title: '',
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          placeholder='Add Todo...'
+          value={this.state.title}
+          name='title'
+          onChange={this.onChange}
+        />
+        <button>Submit</button>
+      </form>
+    );
+  }
 }
 
 export default InputTodo;
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodoList from './TodoList';
 import Header from './Header';
+import InputTodo from './InputTodo';
 
 class TodoContainer extends React.Component {
   state = {
@@ -48,14 +49,28 @@ class TodoContainer extends React.Component {
       ],
     });
   };
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
   render() {
     return (
       <div className='todo-container'>
         <Header />
+        <InputTodo addTodoProps={this.addTodoItem} />
         <TodoList
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
           deleteTodoProps={this.delTodo}
+          addTodoProps={this.addTodoItem}
         />
       </div>
     );
